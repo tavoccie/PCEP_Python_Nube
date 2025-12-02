54fa10dc4923d9c1e6ce63c395e9a59251767b00
tavoccie <gustavo.elf.chile.2019@gmail.com>
2025-12-02 00:48:44 -0300

    Material para PCEP

---
+++ PCEP_Python_Local/Dia 1 - Trabajar con variables en Python para gestionar datos/1-funcion print.py
@@ -0,0 +1,15 @@
+#funcion print() no retorna ningún valor, solo muestra algo en pantalla.
+
+#funcion print() y forma de usar comillas dobles y simples.
+
+print('Day 1 - Python Print Function')
+
+print("The function is declared like this")
+
+#usando \ para decirle a Python que es un apostrofe ' y en string no termina alli.
+print('today It\'s raininig')
+
+# Evitar usar \ si es que se puede evitar.
+print("Today It's raninig")
+
+print("print('what to print')")
